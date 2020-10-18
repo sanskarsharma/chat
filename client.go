d2cfb0919b1453d176a3f66210e76bebfd4b7e7a
sanskarsharma <sanskar2996@gmail.com>
2020-10-18 09:52:09 +0530

    init : basic stateless websocket chat, multi room

---
+++ client.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// connection is an middleman between the websocket connection and the hub.
+type Subscriber struct {
+	// The websocket connection.
+	connection *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+// write writes a message with the given message type and payload.
+func (c *Subscriber) write(mt int, payload []byte) error {
+	c.connection.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.connection.WriteMessage(mt, payload)
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+func (roomSubscription RoomSubscription) readPump() {
+	subscriber := roomSubscription.subscriber
+	defer func() {
+		h.unregister <- roomSubscription
+		subscriber.connection.Close()
+	}()
+	subscriber.connection.SetReadLimit(maxMessageSize)
+	subscriber.connection.SetReadDeadline(time.Now().Add(pongWait))
+	subscriber.connection.SetPongHandler(func(string) error { subscriber.connection.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, msg, err := subscriber.connection.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		m := message{msg, roomSubscription.roomId, ""} // TODO : add senderId 
+		h.broadcast <- m
+	}
+}
+
+
+
+// writePump pumps messages from the hub to the websocket connection.
+func (roomSubscription *RoomSubscription) writePump() {
+	subscriber := roomSubscription.subscriber
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		subscriber.connection.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-subscriber.send:
+			if !ok {
+				subscriber.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := subscriber.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := subscriber.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func serveWs(w http.ResponseWriter, r *http.Request, roomId string) {
+	fmt.Print(roomId)
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	subscriber := &Subscriber{send: make(chan []byte, 256), connection: ws}
+	roomSubscription := RoomSubscription{subscriber, roomId}
+	h.register <- roomSubscription
+	go roomSubscription.writePump()
+	go roomSubscription.readPump()
+}
---
+++ hub.go
@@ -0,0 +1,73 @@
+package main
+
+type message struct {
+	data []byte
+	roomId string
+	senderId string
+}
+
+type RoomSubscription struct {
+	subscriber *Subscriber
+	roomId string
+}
+
+// hub maintains the set of active connections and broadcasts messages to the
+// connections.
+type hub struct {
+	// Registered connections.
+	rooms map[string]map[*Subscriber]bool
+
+	// Inbound messages from the connections.
+	broadcast chan message
+
+	// Register requests from the connections.
+	register chan RoomSubscription
+
+	// Unregister requests from connections.
+	unregister chan RoomSubscription
+}
+
+var h = hub{
+	broadcast:  make(chan message),
+	register:   make(chan RoomSubscription),
+	unregister: make(chan RoomSubscription),
+	rooms:      make(map[string]map[*Subscriber]bool),
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case roomSubscription := <-h.register:
+			subscribers := h.rooms[roomSubscription.roomId]
+			if subscribers == nil {
+				subscribers = make(map[*Subscriber]bool)
+				h.rooms[roomSubscription.roomId] = subscribers
+			}
+			h.rooms[roomSubscription.roomId][roomSubscription.subscriber] = true
+		case roomSubscription := <-h.unregister:
+			subscribers := h.rooms[roomSubscription.roomId]
+			if subscribers != nil {
+				if _, ok := subscribers[roomSubscription.subscriber]; ok {
+					delete(subscribers, roomSubscription.subscriber)
+					close(roomSubscription.subscriber.send)
+					if len(subscribers) == 0 {
+						delete(h.rooms, roomSubscription.roomId)
+					}
+				}
+			}
+		case m := <-h.broadcast:
+			subscribers := h.rooms[m.roomId]
+			for c := range subscribers {
+				select {
+				case c.send <- m.data:
+				default:
+					close(c.send)
+					delete(subscribers, c)
+					if len(subscribers) == 0 {
+						delete(h.rooms, m.roomId)
+					}
+				}
+			}
+		}
+	}
+}
